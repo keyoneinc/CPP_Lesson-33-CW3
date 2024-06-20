c80b63257608e559951fe3e2f361667f34af4695
Yaroslav <yaroslav@keyoneinc.uk>
2024-06-20 18:18:36 +0100

    added definition of class SecondsPlan, in SecondsPlan.cpp

--- cw33-task3/SecondsPlan.cpp
+++ cw33-task3/SecondsPlan.cpp
@@ -2,26 +2,32 @@
 
 Mobile::SecondsPlan::SecondsPlan()
 {
+    priceOfSecond = 0;
 }
 
 Mobile::SecondsPlan::SecondsPlan(string name, float priceOfSecond)
 {
+    this->name = name;
+    this->priceOfSecond = priceOfSecond;
 }
 
 void Mobile::SecondsPlan::setPriceOfSecond(float priceOfSecond)
 {
+    this->priceOfSecond = priceOfSecond;
 }
 
 float Mobile::SecondsPlan::getPriceOfSecond() const
 {
-    return 0.0f;
+    return priceOfSecond;
 }
 
 void Mobile::SecondsPlan::showInfo() const
 {
+    cout << "Plan name: " << name << endl;
+    cout << "Price per second: " << priceOfSecond << endl << endl;
 }
 
 double Mobile::SecondsPlan::calcPrice(int seconds) const
 {
-    return 0.0;
+    return (seconds * priceOfSecond);
 }
