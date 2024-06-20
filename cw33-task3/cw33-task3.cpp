85fabfa0e4517d7d607d74d2c8a0f9ef5736c71a
Yaroslav <yaroslav@keyoneinc.uk>
2024-06-20 18:56:18 +0100

    added example of plans in main.cpp

--- cw33-task3/cw33-task3.cpp
+++ cw33-task3/cw33-task3.cpp
@@ -1,5 +1,13 @@
 #include <iostream>
+#include "Provider.h"
 
 int main() {
-    
+    Mobile::Provider Vodafone("Vodafone");
+    Vodafone.addPlan(new Mobile::SecondsPlan("Student", 0.01));
+    Vodafone.addPlan(new Mobile::MinutesPlan("Red Mini", 0.5));
+    Vodafone.addPlan(new Mobile::SecondsPlan("Red Maxi", 0.05));
+    Vodafone.addPlan(new Mobile::MinutesPlan("Red Top", 0.3));
+
+    Vodafone.showList();
+
 }
