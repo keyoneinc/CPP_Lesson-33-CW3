bcb4c473bc3f8a202b9d6c113df7e8ced02f263c
Yaroslav <yaroslav@keyoneinc.uk>
2024-06-20 19:58:55 +0100

    added makeCall method in consumer.cpp

--- cw33-task3/Consumer.cpp
+++ cw33-task3/Consumer.cpp
@@ -2,16 +2,40 @@
 
 Mobile::Consumer::Consumer()
 {
+	name = "undefined";
+	phone = "none";
+	cash = 0;
+	plan = nullptr;
 }
 
 Mobile::Consumer::Consumer(string name, string phone, double cash, Plan* plan)
 {
+	this->name = name;
+	this->phone = phone;
+	this->cash = cash;
+	this->plan = plan;
 }
 
 void Mobile::Consumer::showInfo() const
 {
+	cout << "Name: " << name << endl;
+	cout << "Phone: " << phone << endl;
+	cout << "Cash: " << cash << endl;
+	if (plan == nullptr) {
+		cout << "Consumer hasn't got an active plan" << endl;
+	}
+	else {
+		plan->showInfo();
+	}
 }
 
 void Mobile::Consumer::makeCall(int seconds)
 {
+	try {
+		float p = plan->calcPrice(seconds);
+		cash -= p;
+	}
+	catch (const std::exception* ne) {
+		cout << ne->what() << endl;
+	}
 }
